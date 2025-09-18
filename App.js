f54a9f21e7ec589ba203c01a59abbe5430454c2d
Kaushalprajapti <kaushalprajapatite@gmail.com>
2025-09-18 17:04:01 +0530

    fix:video rendering

--- App.js
+++ App.js
@@ -1,13 +1,26 @@
 import React from 'react';
+import { StyleSheet } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+
 import { AppProvider } from './src/context/AppContext';
 import AppNavigator from './src/navigation/AppNavigator';
 
 const App = () => {
   return (
-    <AppProvider>
-      <AppNavigator />
-    </AppProvider>
+    // Wrap the entire app in GestureHandlerRootView
+    <GestureHandlerRootView style={styles.container}>
+      <AppProvider>
+        <AppNavigator />
+      </AppProvider>
+    </GestureHandlerRootView>
   );
 };
+
+// It's important to add flex: 1 to the root view
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
 export default App;
